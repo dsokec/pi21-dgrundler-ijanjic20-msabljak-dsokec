fab404eb43932c58181584fc990621c80f567484
dsokec3110 <dsokec@foi.hr>
2021-04-20 12:35:57 +0200

    Test program

---
+++ Itronic/Itronic/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Itronic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pozdravna poruka
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
