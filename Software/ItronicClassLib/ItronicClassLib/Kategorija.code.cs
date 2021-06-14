d5e6cf09af33f642515692d36c4841baabf8943a
dgrundler97 <59441416+dgrundler97@users.noreply.github.com>
2021-06-14 09:11:17 +0200

    dodavanje u klasu

---
+++ Software/ItronicClassLib/ItronicClassLib/Kategorija.code.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItronicClassLib
+{
+    public partial class Kategorija
+    {
+        public override string ToString()
+        {
+            return Naziv;
+        }
+    }
+}
