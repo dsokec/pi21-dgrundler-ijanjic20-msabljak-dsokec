4ea6983619a0f05c16d6d5d23f9794e150bb6466
dgrundler <domi.grund@gmail.com>
2021-06-15 23:50:28 +0200

    Izrada formi

--- Software/Itronic/FrmKupac.cs
+++ Software/Itronic/FrmKupac.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Itronic
 {
-    public partial class Form1 : Form
+    public partial class FrmKupac : Form
     {
-        public Form1()
+        public FrmKupac()
         {
             InitializeComponent();
         }
