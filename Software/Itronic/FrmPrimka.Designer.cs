2b2541257b09c66d74b7c1637637a3c2f8d734eb
dgrundler <domi.grund@gmail.com>
2021-06-16 03:40:37 +0200

    Dizajn + metode forme za naručivanje zaliha

--- Software/Itronic/FrmPrimka.Designer.cs
+++ Software/Itronic/FrmPrimka.Designer.cs
@@ -29,12 +29,196 @@ namespace Itronic
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnFiltriraj = new System.Windows.Forms.Button();
+            this.btnMakniFiltriranje = new System.Windows.Forms.Button();
+            this.cmbKategorija = new System.Windows.Forms.ComboBox();
+            this.lblKategorija = new System.Windows.Forms.Label();
+            this.btnOdustani = new System.Windows.Forms.Button();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.dgvPrimka = new System.Windows.Forms.DataGridView();
+            this.lblUkupnaCijenaIznos = new System.Windows.Forms.Label();
+            this.lblUkupnaCijena = new System.Windows.Forms.Label();
+            this.btnNaruci = new System.Windows.Forms.Button();
+            this.txtKolicina = new System.Windows.Forms.TextBox();
+            this.lblKolicina = new System.Windows.Forms.Label();
+            this.dgvKatalog = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrimka)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKatalog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFiltriraj
+            // 
+            this.btnFiltriraj.Location = new System.Drawing.Point(244, 13);
+            this.btnFiltriraj.Name = "btnFiltriraj";
+            this.btnFiltriraj.Size = new System.Drawing.Size(119, 29);
+            this.btnFiltriraj.TabIndex = 28;
+            this.btnFiltriraj.Text = "Filtriraj";
+            this.btnFiltriraj.UseVisualStyleBackColor = true;
+            this.btnFiltriraj.Click += new System.EventHandler(this.btnFiltriraj_Click);
+            // 
+            // btnMakniFiltriranje
+            // 
+            this.btnMakniFiltriranje.Location = new System.Drawing.Point(369, 13);
+            this.btnMakniFiltriranje.Name = "btnMakniFiltriranje";
+            this.btnMakniFiltriranje.Size = new System.Drawing.Size(119, 29);
+            this.btnMakniFiltriranje.TabIndex = 27;
+            this.btnMakniFiltriranje.Text = "Makni filtriranje";
+            this.btnMakniFiltriranje.UseVisualStyleBackColor = true;
+            this.btnMakniFiltriranje.Click += new System.EventHandler(this.btnMakniFiltriranje_Click);
+            // 
+            // cmbKategorija
+            // 
+            this.cmbKategorija.FormattingEnabled = true;
+            this.cmbKategorija.Location = new System.Drawing.Point(117, 16);
+            this.cmbKategorija.Name = "cmbKategorija";
+            this.cmbKategorija.Size = new System.Drawing.Size(121, 24);
+            this.cmbKategorija.TabIndex = 26;
+            // 
+            // lblKategorija
+            // 
+            this.lblKategorija.AutoSize = true;
+            this.lblKategorija.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblKategorija.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblKategorija.Location = new System.Drawing.Point(19, 22);
+            this.lblKategorija.Name = "lblKategorija";
+            this.lblKategorija.Size = new System.Drawing.Size(89, 18);
+            this.lblKategorija.TabIndex = 25;
+            this.lblKategorija.Text = "Kategorija:";
+            // 
+            // btnOdustani
+            // 
+            this.btnOdustani.Location = new System.Drawing.Point(518, 529);
+            this.btnOdustani.Name = "btnOdustani";
+            this.btnOdustani.Size = new System.Drawing.Size(82, 32);
+            this.btnOdustani.TabIndex = 24;
+            this.btnOdustani.Text = "Odustani";
+            this.btnOdustani.UseVisualStyleBackColor = true;
+            this.btnOdustani.Click += new System.EventHandler(this.btnOdustani_Click);
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(218, 280);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(75, 23);
+            this.btnDodaj.TabIndex = 23;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            this.btnDodaj.Click += new System.EventHandler(this.btnDodaj_Click);
+            // 
+            // dgvPrimka
+            // 
+            this.dgvPrimka.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPrimka.Location = new System.Drawing.Point(12, 320);
+            this.dgvPrimka.Name = "dgvPrimka";
+            this.dgvPrimka.RowHeadersWidth = 51;
+            this.dgvPrimka.RowTemplate.Height = 24;
+            this.dgvPrimka.Size = new System.Drawing.Size(676, 198);
+            this.dgvPrimka.TabIndex = 22;
+            // 
+            // lblUkupnaCijenaIznos
+            // 
+            this.lblUkupnaCijenaIznos.AutoSize = true;
+            this.lblUkupnaCijenaIznos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblUkupnaCijenaIznos.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblUkupnaCijenaIznos.Location = new System.Drawing.Point(195, 532);
+            this.lblUkupnaCijenaIznos.Name = "lblUkupnaCijenaIznos";
+            this.lblUkupnaCijenaIznos.Size = new System.Drawing.Size(17, 18);
+            this.lblUkupnaCijenaIznos.TabIndex = 21;
+            this.lblUkupnaCijenaIznos.Text = "0";
+            // 
+            // lblUkupnaCijena
+            // 
+            this.lblUkupnaCijena.AutoSize = true;
+            this.lblUkupnaCijena.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblUkupnaCijena.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblUkupnaCijena.Location = new System.Drawing.Point(19, 532);
+            this.lblUkupnaCijena.Name = "lblUkupnaCijena";
+            this.lblUkupnaCijena.Size = new System.Drawing.Size(144, 18);
+            this.lblUkupnaCijena.TabIndex = 20;
+            this.lblUkupnaCijena.Text = "UKUPNA CIJENA:";
+            // 
+            // btnNaruci
+            // 
+            this.btnNaruci.Location = new System.Drawing.Point(606, 528);
+            this.btnNaruci.Name = "btnNaruci";
+            this.btnNaruci.Size = new System.Drawing.Size(82, 33);
+            this.btnNaruci.TabIndex = 19;
+            this.btnNaruci.Text = "Naruči";
+            this.btnNaruci.UseVisualStyleBackColor = true;
+            this.btnNaruci.Click += new System.EventHandler(this.btnNaruci_Click);
+            // 
+            // txtKolicina
+            // 
+            this.txtKolicina.Location = new System.Drawing.Point(91, 281);
+            this.txtKolicina.Name = "txtKolicina";
+            this.txtKolicina.Size = new System.Drawing.Size(100, 22);
+            this.txtKolicina.TabIndex = 18;
+            // 
+            // lblKolicina
+            // 
+            this.lblKolicina.AutoSize = true;
+            this.lblKolicina.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblKolicina.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblKolicina.Location = new System.Drawing.Point(12, 285);
+            this.lblKolicina.Name = "lblKolicina";
+            this.lblKolicina.Size = new System.Drawing.Size(73, 18);
+            this.lblKolicina.TabIndex = 17;
+            this.lblKolicina.Text = "Količina:";
+            // 
+            // dgvKatalog
+            // 
+            this.dgvKatalog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKatalog.Location = new System.Drawing.Point(12, 49);
+            this.dgvKatalog.Name = "dgvKatalog";
+            this.dgvKatalog.RowHeadersWidth = 51;
+            this.dgvKatalog.RowTemplate.Height = 24;
+            this.dgvKatalog.Size = new System.Drawing.Size(676, 209);
+            this.dgvKatalog.TabIndex = 16;
+            this.dgvKatalog.SelectionChanged += new System.EventHandler(this.dgvKatalog_SelectionChanged);
+            // 
+            // FrmPrimka
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmPrimka";
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(712, 578);
+            this.Controls.Add(this.btnFiltriraj);
+            this.Controls.Add(this.btnMakniFiltriranje);
+            this.Controls.Add(this.cmbKategorija);
+            this.Controls.Add(this.lblKategorija);
+            this.Controls.Add(this.btnOdustani);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.dgvPrimka);
+            this.Controls.Add(this.lblUkupnaCijenaIznos);
+            this.Controls.Add(this.lblUkupnaCijena);
+            this.Controls.Add(this.btnNaruci);
+            this.Controls.Add(this.txtKolicina);
+            this.Controls.Add(this.lblKolicina);
+            this.Controls.Add(this.dgvKatalog);
+            this.Name = "FrmPrimka";
+            this.Text = "Naručivanje zaliha";
+            this.Load += new System.EventHandler(this.FrmPrimka_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrimka)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKatalog)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnFiltriraj;
+        private System.Windows.Forms.Button btnMakniFiltriranje;
+        private System.Windows.Forms.ComboBox cmbKategorija;
+        private System.Windows.Forms.Label lblKategorija;
+        private System.Windows.Forms.Button btnOdustani;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.DataGridView dgvPrimka;
+        private System.Windows.Forms.Label lblUkupnaCijenaIznos;
+        private System.Windows.Forms.Label lblUkupnaCijena;
+        private System.Windows.Forms.Button btnNaruci;
+        private System.Windows.Forms.TextBox txtKolicina;
+        private System.Windows.Forms.Label lblKolicina;
+        private System.Windows.Forms.DataGridView dgvKatalog;
     }
 }
--- Software/Itronic/FrmPrimka.cs
+++ Software/Itronic/FrmPrimka.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ItronicClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,145 @@ using System.Windows.Forms;
 
 namespace Itronic
 {
+    /// <summary>
+    /// Autor: Domagoj Grundler
+    /// </summary>
     public partial class FrmPrimka : Form
     {
+        private DataTable kosarica;
+        public double ukupnaCijena;
+        private Proizvod odabraniProizvod;
+        private List<Proizvod> odabraniProizvodi;
+        private List<int> kolicine;
         public FrmPrimka()
         {
             InitializeComponent();
+            InicijalizirajKosaricu();
+            ukupnaCijena = 0;
+            odabraniProizvodi = new List<Proizvod>();
+            kolicine = new List<int>();
+        }
+
+        private void InicijalizirajKosaricu()
+        {
+            kosarica = new DataTable();
+            kosarica.Columns.Add("Naziv", typeof(string));
+            kosarica.Columns.Add("Cijena", typeof(float));
+            kosarica.Columns.Add("Kolicina", typeof(int));
+            kosarica.Columns.Add("Iznos", typeof(float));
+        }
+
+        private void FrmPrimka_Load(object sender, EventArgs e)
+        {
+            PrikaziProizvodeIKosaricu();
+            PopuniKategorije();
+        }
+
+        private void PopuniKategorije()
+        {
+            cmbKategorija.DataSource = Repository.DohvatiSveKategorije();
+        }
+
+        private void PrikaziProizvodeIKosaricu()
+        {
+            dgvKatalog.DataSource = null;
+            dgvKatalog.DataSource = Repository.DohvatiSveProizvode();
+            dgvKatalog.Columns["Kategorija"].Visible = false;
+            dgvKatalog.Columns["Recenzijas"].Visible = false;
+            dgvKatalog.Columns["Reklamacijas"].Visible = false;
+            dgvKatalog.Columns["StavkaPrimkes"].Visible = false;
+            dgvKatalog.Columns["StavkaRacunas"].Visible = false;
+            dgvKatalog.Columns["ID"].Visible = false;
+            dgvKatalog.Columns["KategorijaID"].Visible = false;
+            dgvKatalog.Columns["ProdajnaCijena"].Visible = false;
+
+            dgvPrimka.DataSource = null;
+            dgvPrimka.DataSource = kosarica;
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            if (odabraniProizvod == null)
+            {
+                MessageBox.Show("Morate prvo odabrati proizvod!");
+                return;
+            }
+            DodajUKosaricu();
+        }
+
+        private void DodajUKosaricu()
+        {
+            string naziv = odabraniProizvod.Naziv;
+            double cijena = odabraniProizvod.KupovnaCIjena;
+            if (txtKolicina.Text == "")
+            {
+                MessageBox.Show("Morate unijeti količinu!");
+                return;
+            }
+            int kolicina = int.Parse(txtKolicina.Text);
+            double iznos = cijena * kolicina;
+
+            if (odabraniProizvodi.Contains(odabraniProizvod))
+            {
+                int index = odabraniProizvodi.IndexOf(odabraniProizvod);
+                kolicina += kolicine[index];
+                kolicine[index] = kolicina;
+                iznos = cijena * kolicina;
+                kosarica.Rows.RemoveAt(index);
+                kosarica.Rows.Add(naziv, cijena, kolicina, iznos);
+                AzurirajKosaricu(iznos);
+            }
+            else
+            {
+                odabraniProizvodi.Add(odabraniProizvod);
+                kolicine.Add(kolicina);
+
+                kosarica.Rows.Add(naziv, cijena, kolicina, iznos);
+                AzurirajKosaricu(iznos);
+            }
+        }
+
+        private void AzurirajKosaricu(double iznos)
+        {
+            dgvPrimka.DataSource = null;
+            dgvPrimka.DataSource = kosarica;
+
+            ukupnaCijena += iznos;
+            lblUkupnaCijenaIznos.Text = ukupnaCijena.ToString();
+        }
+
+        private void btnNaruci_Click(object sender, EventArgs e)
+        {
+            if (odabraniProizvodi.Count == 0 || kolicine.Count == 0)
+            {
+                MessageBox.Show("Niste stavili niti jedan proizvod u Vašu košaricu!");
+                return;
+            }
+            Repository.PosaljiNarudzbenicu(odabraniProizvodi, kolicine);
+            Close();
+        }
+
+        private void btnFiltriraj_Click(object sender, EventArgs e)
+        {
+            Kategorija kategorija = cmbKategorija.SelectedItem as Kategorija;
+            dgvKatalog.DataSource = null;
+            dgvKatalog.DataSource = Repository.DohvatiProizvodePoKategoriji(kategorija);
+        }
+
+        private void btnMakniFiltriranje_Click(object sender, EventArgs e)
+        {
+            dgvKatalog.DataSource = null;
+            dgvKatalog.DataSource = Repository.DohvatiSveProizvode();
+        }
+
+        private void btnOdustani_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void dgvKatalog_SelectionChanged(object sender, EventArgs e)
+        {
+            odabraniProizvod = dgvKatalog.CurrentRow.DataBoundItem as Proizvod;
         }
     }
 }
