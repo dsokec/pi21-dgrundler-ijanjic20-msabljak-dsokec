9406d2c5cee45f36d8ef6846b365dc7ff7e41e6c
dsokec3110 <dsokec@foi.hr>
2022-06-05 14:49:22 +0200

    Login Windows Forma

---
+++ Software/Dino_Sokec/ItronicStore/LoginWF.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt2022
+{
+    public partial class wfLogin : Form
+    {
+        public wfLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrijaviSe_Click(object sender, EventArgs e)
+        {
+            string korisnickoIme = txtKorisnickoIme.Text;
+            string lozinka = txtLozinka.Text;
+
+            // Poruke upozorenja
+            string podatciPoruka = "Unesite podatke za prijavu";
+            string uspjehPoruka = "Uspjesno ste prijavljeni";
+            string neispravnoPoruka = "Neispravno korisnicko ime ili lozinka";
+
+            string g = "Greska";
+            string u = "Uspjeh";
+
+            if (string.IsNullOrEmpty(korisnickoIme) || string.IsNullOrEmpty(lozinka))
+            {
+                MessageBox.Show(podatciPoruka, g, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (ProvjeriPodatkeNaServeru(korisnickoIme, lozinka))
+            {
+                MessageBox.Show(uspjehPoruka, u, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(neispravnoPoruka, g, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private bool ProvjeriPodatkeNaServeru(string korisnik, string pass)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnRegistrirajSe_Click(object sender, EventArgs e)
+        {
+            OtvoriWindowsFormuRegistracija();
+        }
+
+        private void OtvoriWindowsFormuRegistracija()
+        {
+            this.Hide();
+            RegistrationWF registration = new RegistrationWF();
+            registration.ShowDialog();
+
+            this.Show();
+        }
+
+        private void btnIzlaz_Click(object sender, EventArgs e)
+        {
+            NapustanjeAplikacije();
+        }
+
+        private void NapustanjeAplikacije()
+        {
+            string upit = "Jeste li sigurni ?";
+
+            DialogResult odgovor = MessageBox.Show(upit, "Izlaz", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            
+            if(odgovor == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void wfLogin_Load(object sender, EventArgs e)
+        {
+            ZapocniUnositiPodatke();
+        }
+
+        private void ZapocniUnositiPodatke()
+        {
+            txtKorisnickoIme.Focus();
+        }
+
+        private void wfLogin_HelpRequested(object sender, HelpEventArgs hlpevent)
+        {
+            OtvoriHelpFile(6000);
+        }
+
+        private void OtvoriHelpFile(int id = -1)
+        {
+            string help = PutanjaDoHelpFile();
+            if (id == -1)
+            {
+                //otvara se naslovna stranicu u help file
+                System.Diagnostics.Process.Start(help);
+            }
+            else
+            {
+                //otvora se trazena stranica u help file
+                Help.ShowHelp(this, help, HelpNavigator.TopicId, id.ToString());
+            }
+        }
+
+        private string PutanjaDoHelpFile()
+        {
+            // absolute path
+            string putanja = Path.Combine(new Uri(Path.GetDirectoryName
+            (Assembly.GetExecutingAssembly().CodeBase)).LocalPath, "help.chm");
+
+            return putanja;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chkPrikaziLozinku.Checked == true)
+            {
+                txtLozinka.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtLozinka.UseSystemPasswordChar = true;
+            }
+        }
+    }
+}
---
+++ Software/Dino_Sokec/ItronicStore/LoginWF.designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Projekt2022
+{
+    partial class wfLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKorisnickoIme = new System.Windows.Forms.TextBox();
+            this.txtLozinka = new System.Windows.Forms.TextBox();
+            this.btnPrijaviSe = new System.Windows.Forms.Button();
+            this.btnNastaviKaoGost = new System.Windows.Forms.Button();
+            this.lblKorisnickoIme = new System.Windows.Forms.Label();
+            this.lblLozinka = new System.Windows.Forms.Label();
+            this.lblNaslov = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRegistrirajSe = new System.Windows.Forms.Button();
+            this.btnIzlaz = new System.Windows.Forms.Button();
+            this.chkPrikaziLozinku = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtKorisnickoIme
+            // 
+            this.txtKorisnickoIme.Location = new System.Drawing.Point(140, 152);
+            this.txtKorisnickoIme.Name = "txtKorisnickoIme";
+            this.txtKorisnickoIme.Size = new System.Drawing.Size(183, 26);
+            this.txtKorisnickoIme.TabIndex = 0;
+            // 
+            // txtLozinka
+            // 
+            this.txtLozinka.Location = new System.Drawing.Point(140, 204);
+            this.txtLozinka.Name = "txtLozinka";
+            this.txtLozinka.Size = new System.Drawing.Size(183, 26);
+            this.txtLozinka.TabIndex = 1;
+            this.txtLozinka.UseSystemPasswordChar = true;
+            // 
+            // btnPrijaviSe
+            // 
+            this.btnPrijaviSe.Location = new System.Drawing.Point(55, 276);
+            this.btnPrijaviSe.Name = "btnPrijaviSe";
+            this.btnPrijaviSe.Size = new System.Drawing.Size(133, 52);
+            this.btnPrijaviSe.TabIndex = 2;
+            this.btnPrijaviSe.Text = "Prijavi se";
+            this.btnPrijaviSe.UseVisualStyleBackColor = true;
+            this.btnPrijaviSe.Click += new System.EventHandler(this.btnPrijaviSe_Click);
+            // 
+            // btnNastaviKaoGost
+            // 
+            this.btnNastaviKaoGost.Location = new System.Drawing.Point(157, 357);
+            this.btnNastaviKaoGost.Name = "btnNastaviKaoGost";
+            this.btnNastaviKaoGost.Size = new System.Drawing.Size(154, 52);
+            this.btnNastaviKaoGost.TabIndex = 4;
+            this.btnNastaviKaoGost.Text = "Nastavi kao gost";
+            this.btnNastaviKaoGost.UseVisualStyleBackColor = true;
+            // 
+            // lblKorisnickoIme
+            // 
+            this.lblKorisnickoIme.AutoSize = true;
+            this.lblKorisnickoIme.Location = new System.Drawing.Point(20, 155);
+            this.lblKorisnickoIme.Name = "lblKorisnickoIme";
+            this.lblKorisnickoIme.Size = new System.Drawing.Size(114, 20);
+            this.lblKorisnickoIme.TabIndex = 2;
+            this.lblKorisnickoIme.Text = "Korisničko ime:";
+            // 
+            // lblLozinka
+            // 
+            this.lblLozinka.AutoSize = true;
+            this.lblLozinka.Location = new System.Drawing.Point(66, 204);
+            this.lblLozinka.Name = "lblLozinka";
+            this.lblLozinka.Size = new System.Drawing.Size(68, 20);
+            this.lblLozinka.TabIndex = 2;
+            this.lblLozinka.Text = "Lozinka:";
+            // 
+            // lblNaslov
+            // 
+            this.lblNaslov.AutoSize = true;
+            this.lblNaslov.Location = new System.Drawing.Point(176, 101);
+            this.lblNaslov.Name = "lblNaslov";
+            this.lblNaslov.Size = new System.Drawing.Size(113, 20);
+            this.lblNaslov.TabIndex = 2;
+            this.lblNaslov.Text = "Itronic trgovina";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(180, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnRegistrirajSe
+            // 
+            this.btnRegistrirajSe.Location = new System.Drawing.Point(277, 276);
+            this.btnRegistrirajSe.Name = "btnRegistrirajSe";
+            this.btnRegistrirajSe.Size = new System.Drawing.Size(137, 52);
+            this.btnRegistrirajSe.TabIndex = 3;
+            this.btnRegistrirajSe.Text = "Registriraj se";
+            this.btnRegistrirajSe.UseVisualStyleBackColor = true;
+            this.btnRegistrirajSe.Click += new System.EventHandler(this.btnRegistrirajSe_Click);
+            // 
+            // btnIzlaz
+            // 
+            this.btnIzlaz.Location = new System.Drawing.Point(397, 418);
+            this.btnIzlaz.Name = "btnIzlaz";
+            this.btnIzlaz.Size = new System.Drawing.Size(66, 40);
+            this.btnIzlaz.TabIndex = 5;
+            this.btnIzlaz.Text = "Izlaz";
+            this.btnIzlaz.UseVisualStyleBackColor = true;
+            this.btnIzlaz.Click += new System.EventHandler(this.btnIzlaz_Click);
+            // 
+            // chkPrikaziLozinku
+            // 
+            this.chkPrikaziLozinku.AutoSize = true;
+            this.chkPrikaziLozinku.Location = new System.Drawing.Point(330, 205);
+            this.chkPrikaziLozinku.Name = "chkPrikaziLozinku";
+            this.chkPrikaziLozinku.Size = new System.Drawing.Size(134, 24);
+            this.chkPrikaziLozinku.TabIndex = 6;
+            this.chkPrikaziLozinku.Text = "Prikaži lozinku";
+            this.chkPrikaziLozinku.UseVisualStyleBackColor = true;
+            this.chkPrikaziLozinku.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // wfLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 480);
+            this.Controls.Add(this.chkPrikaziLozinku);
+            this.Controls.Add(this.btnIzlaz);
+            this.Controls.Add(this.btnRegistrirajSe);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblLozinka);
+            this.Controls.Add(this.lblNaslov);
+            this.Controls.Add(this.lblKorisnickoIme);
+            this.Controls.Add(this.btnNastaviKaoGost);
+            this.Controls.Add(this.btnPrijaviSe);
+            this.Controls.Add(this.txtLozinka);
+            this.Controls.Add(this.txtKorisnickoIme);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "wfLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.wfLogin_Load);
+            this.HelpRequested += new System.Windows.Forms.HelpEventHandler(this.wfLogin_HelpRequested);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKorisnickoIme;
+        private System.Windows.Forms.TextBox txtLozinka;
+        private System.Windows.Forms.Button btnPrijaviSe;
+        private System.Windows.Forms.Button btnNastaviKaoGost;
+        private System.Windows.Forms.Label lblKorisnickoIme;
+        private System.Windows.Forms.Label lblLozinka;
+        private System.Windows.Forms.Label lblNaslov;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRegistrirajSe;
+        private System.Windows.Forms.Button btnIzlaz;
+        private System.Windows.Forms.CheckBox chkPrikaziLozinku;
+    }
+}
+
