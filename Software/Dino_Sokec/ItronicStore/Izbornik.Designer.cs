5146188acc545aec556eee2dcc758d033a667784
dsokec3110 <dsokec@foi.hr>
2022-06-13 22:01:21 +0200

    Izbornik

---
+++ Software/Dino_Sokec/ItronicStore/Izbornik.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace ItronicStore
+{
+    partial class Izbornik
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnRecenzija = new System.Windows.Forms.Button();
+            this.btnReklamacija = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(315, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Trgovina Itronic";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Način uporabe";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Odaberite željenu opciju";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(314, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Podnesite reklamaciju na odabrani proizvod";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Ostavite recenziju";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(569, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Vi ste prijavljeni kao";
+            // 
+            // btnRecenzija
+            // 
+            this.btnRecenzija.Location = new System.Drawing.Point(146, 268);
+            this.btnRecenzija.Name = "btnRecenzija";
+            this.btnRecenzija.Size = new System.Drawing.Size(91, 41);
+            this.btnRecenzija.TabIndex = 1;
+            this.btnRecenzija.Text = "Recenzija";
+            this.btnRecenzija.UseVisualStyleBackColor = true;
+            // 
+            // btnReklamacija
+            // 
+            this.btnReklamacija.Location = new System.Drawing.Point(346, 268);
+            this.btnReklamacija.Name = "btnReklamacija";
+            this.btnReklamacija.Size = new System.Drawing.Size(103, 41);
+            this.btnReklamacija.TabIndex = 1;
+            this.btnReklamacija.Text = "Reklamacija";
+            this.btnReklamacija.UseVisualStyleBackColor = true;
+            // 
+            // Izbornik
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReklamacija);
+            this.Controls.Add(this.btnRecenzija);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Izbornik";
+            this.Text = "Izbornik";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnRecenzija;
+        private System.Windows.Forms.Button btnReklamacija;
+    }
+}
